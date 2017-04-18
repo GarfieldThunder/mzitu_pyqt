b20a9df132e689e604ccdadeed0d0f85238c6b47
GarfieldThunder <ctrl.xiaobin@gmail.com>
2017-04-18 22:39:09 +0800

    add pager

--- spyder.py
+++ spyder.py
@@ -15,13 +15,19 @@ ui_file = 'mainwindowmz.ui'
 (class_ui, class_basic_class) = PyQt5.uic.loadUiType(ui_file)
 
 class Window(class_basic_class, class_ui):
+    
+    
     def __init__(self):
         super(Window, self).__init__()
+        self.url = "http://www.mzitu.com/"
         self.setupUi(self)
-        totlist = Mzitu().printli()
+        totlist = Mzitu(self.url).printli()
+        self.page = 1
         self.checkBoxs = [self._addCheckbox(item[0], item[1]) for item in totlist]
-        self.pusjButtons = [self._addpushButtonpic(item[0], item[4]) for item in totlist]
+        self.pushButtons = [self._addpushButtonpic(item[0], item[4]) for item in totlist]
         self.pushButton.clicked.connect(self.getSelList)
+        self.pushButton_nextpage.clicked.connect(lambda: self._nextPage(1))
+        self.pushButton_uppage.clicked.connect(lambda: self._nextPage(-1))
         
     def getSelList(self):
         sellist = [(item.objectName(), item.text()) for item in self.checkBoxs if item.isChecked() == True]
@@ -48,14 +54,33 @@ class Window(class_basic_class, class_ui):
         pixmap = QPixmap()
         pixmap.loadFromData(pic)
         self.label.setPixmap(pixmap)
+        
+    def _nextPage(self, page):
+        self.page += page
+        self.textBrowser.append("现在是第%d页" % self.page)
+        url = self.url + '/page/' + str(self.page)
+        totlist = Mzitu(url).printli()
+        newcheckBoxs = []
+        newpushButtons = []
+        for c, p, item in zip(self.checkBoxs, self.pushButtons, totlist):
+            newcheckbox = self._addCheckbox(item[0], item[1])
+            newpushbutton = self._addpushButtonpic(item[0], item[4])
+            self.verticalLayout.replaceWidget(c, newcheckbox)
+            self.verticalLayout_pic.replaceWidget(p, newpushbutton)
+            c.deleteLater()
+            p.deleteLater()
+            newcheckBoxs.append(newcheckbox)
+            newpushButtons.append(newpushbutton)
+        self.checkBoxs = newcheckBoxs
+        self.pushButtons = newpushButtons
 
 class Mzitu():
     
-    def __init__(self):
-        url = "http://www.mzitu.com/"
+    def __init__(self, url):
+        self.url = url
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
                    'Host': 'www.mzitu.com'}
-        response = requests.get(url, headers = headers)
+        response = requests.get(self.url, headers = headers)
         content = BeautifulSoup(response.text, 'lxml')
         linkblock = content.find('div', class_="postlist")
         self.linklist = linkblock.find_all('li')
